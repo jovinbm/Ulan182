190426b4ad6cc6970598f8929c8bdc116d73bd91
Jovin Mwilanga <jovinbeda@hotmail.com>
2015-10-16 04:20:37 -0400

    fixed db

--- passport/passport.js
+++ passport/passport.js
@@ -454,11 +454,21 @@ module.exports = function (app, passport, LocalStrategy) {
 
         return Promise.resolve()
             .then(function () {
-                var url = 'https://login.uber.com/oauth/v2/authorize?' +
-                    'response_type=code' +
-                    '&redirect_uri=' + encodeURIComponent('http://localhost:7000/api/uberauth/callback') +
-                    '&scope=request history profile' +
-                    '&client_id=' + rq.uber.defaults.client_id;
+
+                var url;
+                if (process.env.NODE_ENV == 'production') {
+                    url = 'https://login.uber.com/oauth/v2/authorize?' +
+                        'response_type=code' +
+                        '&redirect_uri=' + encodeURIComponent('http:s//www.pluschat.net/api/uberauth/callback') +
+                        '&scope=request history profile' +
+                        '&client_id=' + rq.uber.defaults.client_id;
+                } else {
+                    url = 'https://login.uber.com/oauth/v2/authorize?' +
+                        'response_type=code' +
+                        '&redirect_uri=' + encodeURIComponent('http://localhost:7000/api/uberauth/callback') +
+                        '&scope=request history profile' +
+                        '&client_id=' + rq.uber.defaults.client_id;
+                }
 
                 return res.status(200).send({
                     url: url
